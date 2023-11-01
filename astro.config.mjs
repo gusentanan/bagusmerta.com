5960a934c9d1b9093ede5c78ddd010263be6336b
Bagus Merta <merta.bagus24@gmail.com>
2023-11-01 11:36:31 +0800

    fix: added output config on astro.config..mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import markdoc from "@astrojs/markdoc";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gusentanan.github.io',
+  output: "static", // cc: https://docs.astro.build/en/reference/configuration-reference/#output
   integrations: [markdoc()],
   experimental: { assets: true },
+
 });
