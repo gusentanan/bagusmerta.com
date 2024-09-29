6c0c78c7266e2372485753b6ae0949cba3fa78ba
Bagus Merta <merta.bagus24@gmail.com>
2024-09-29 22:56:08 +0800

    feat: add style guide and new blog

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -35,14 +35,6 @@ export default {
 					DEFAULT: 'hsl(var(--secondary))',
 					foreground: 'hsl(var(--secondary-foreground))',
 				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
-				},
-				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
-				},
 				destructive: {
 					DEFAULT: 'hsl(var(--destructive))',
 					foreground: 'hsl(var(--destructive-foreground))',
