3a30b3f1054737dfba7e839afd1ae84dad37a359
Bagus Merta <merta.bagus24@gmail.com>
2025-07-05 08:59:13 +0800

    revert: about page image sizing and blur effect

--- src/components/layout/Drawer.tsx
+++ src/components/layout/Drawer.tsx
@@ -40,6 +40,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       className={cn(
         "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border border-gray-800 bg-background",
+        "transform: translateZ(0)",
         className,
       )}
       ref={ref}
